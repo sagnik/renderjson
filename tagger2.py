a35d1c5720c773774b994e1871e28497a15a7f7f
sagnik <sagnik@ubuntu.ubuntu-domain>
2015-05-05 01:52:45 -0400

    added new code for tagging, use tagger2.py, still getting weird JSONDecode errors

--- tagger2.py
+++ tagger2.py
@@ -1,6 +1,6 @@
 import web
 import sys,os,shutil
-import json 
+import simplejson as json 
 import csv
 
 jsondir='/home/sagnik/linegraph-experiment-1/jsonsimagetextclasspredicted/'#sys.argv[1]
@@ -18,36 +18,48 @@ class index:
         jsondir,jsonfile=os.path.split(jsons[currentindex])
         imgloc=os.path.join(imagedir,jsonfile[:-29]+"-textclassified.png")
         shutil.copy(imgloc,"static")
-	print "currentindex get",currentindex,jsons[currentindex]
+        print "currentindex get",currentindex,jsons[currentindex]
         unlabeledimtext=json.load(open(jsons[currentindex]))['ImageText']
         for index,item in enumerate(unlabeledimtext):
-            unlabeledimtext[index]['TextLabelGold']='textlabelgold-'+unlabeledimtext[index]['TextLabel']	
-	
-        return render.test2(json.dumps(unlabeledimtext),jsonfile[:-29]+"-textclassified.png")
+            unlabeledimtext[index]['TextLabelGold']=unlabeledimtext[index]['TextLabel']	
+        imgbb=json.load(open(jsons[currentindex]))['ImageBB']
+        imgwidth=(imgbb[2]-imgbb[0])*2
+        imgheight=(imgbb[3]-imgbb[1])*2
+        return render.jslabeltagging2(json.dumps(unlabeledimtext),jsonfile[:-29]+"-textclassified.png",\
+        str(imgwidth)+"px",str(imgheight)+"px")
      
     def POST(self):
         inputdata=web.input()
+        print inputdata
         global currentindex
         currentjson=json.load(open(jsons[currentindex]))
         try:
+            #unlabeledimtext=['ImageText']
+            for index,item in enumerate(currentjson['ImageText']):
+                currentjson['ImageText'][index]['TextLabelGold']=currentjson['ImageText'][index]['TextLabel']
             for item in inputdata.keys():
                 currentjson['ImageText'][int(item)]['TextLabelGold']=inputdata[item]
-            currentjson['GoldLabelsTagged']=True
+            #currentjson['GoldLabelsTagged']=True
             json.dump(currentjson,open(jsons[currentindex],"wb"))
             currentindex+=1
             print "success",currentindex
         except (KeyError,IOError,IndexError): 
             print "something wronng happened"
-	
+	return
+	'''
         jsondir,jsonfile=os.path.split(jsons[currentindex])
         imgloc=os.path.join(imagedir,jsonfile[:-29]+"-textclassified.png")
         shutil.copy(imgloc,"static")
         unlabeledimtext=json.load(open(jsons[currentindex]))['ImageText']
         for index,item in enumerate(unlabeledimtext):
-            unlabeledimtext[index]['TextLabelGold']='textlabelgold-'+unlabeledimtext[index]['TextLabel']	
-	
-        return render.test2(json.dumps(unlabeledimtext),jsonfile[:-29]+"-textclassified.png")
-
+            unlabeledimtext[index]['TextLabelGold']=unlabeledimtext[index]['TextLabel']	
+	imgbb=json.load(open(jsons[currentindex]))['ImageBB']
+        imgwidth=(imgbb[2]-imgbb[0])*2
+        imgheight=(imgbb[3]-imgbb[1])*2
+        return render.jslabeltagging2(json.dumps(unlabeledimtext),jsonfile[:-29]+"-textclassified.png",\
+        str(imgwidth)+"px",str(imgheight)+"px")
+        '''
+        
 if __name__=="__main__":
     web.internalerror = web.debugerror
     app = web.application(urls, globals())
